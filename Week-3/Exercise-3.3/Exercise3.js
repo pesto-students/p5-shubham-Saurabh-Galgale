6d45e781d35e69c329a58daf4a409e165c30cfa8
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-10 20:56:56 +0530

    corrected Exercise-3.3

--- Week-3/Exercise-3.3/Exercise3.js
+++ Week-3/Exercise-3.3/Exercise3.js
@@ -1,50 +1,9 @@
-// function createIncrement() {
-//     let count = 0;
-//     function increment() {
-//         count++;
-//     }
-
-
-
-    // message is created once and has nothing to do with increment
-    // thats why showing count is 0 all the Time.
-
-    // message should be inside the increment() so it will change the count
-
-
-
-
-//     let message = `count is ${count}`;
-//     function log() {
-//         console.log(message);
-//     }
-//     return [increment, log];
-// }
-
-// const [increment, log] = createIncrement();
-
-
-
-// we are incrementing 3 times but not returning it any way.
-// increment();
-// increment();
-// increment();
-
-
-// logging the message here.
-// log();
-
-
-// solution to get Count is 3------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 function createIncrement() {
     let count = 0;
-    let message;
     function increment() {
         count++;
-        message = `count is ${count}`;
     }
+    let message = `count is ${count}`;
 
     function log() {
         console.log(message);
