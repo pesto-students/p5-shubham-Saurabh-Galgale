1070584caf688e39a3d013a45d6b44bab250d045
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-11 15:42:35 +0530

    Exercise 4.3 completed

---
+++ Week-4/Exercise-4.3/iterators.js
@@ -0,0 +1,21 @@
+const fib = (n) => ({
+    [Symbol.iterator]: () => {
+        let i = 1;
+        let old = 0, new1 = 0;
+        return {
+            next: () => {
+                if(i++ <= n) {
+                    [old, new1] = [new1, (old + new1) || 1];
+                    // console.log([old, new1]);
+                    return {value: old, done: false}
+                }
+                else {
+                    return {done: true}
+                }
+            }
+        }
+    }
+});
+
+
+console.log([...fib(20) ]);
