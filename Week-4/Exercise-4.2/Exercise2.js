bce972ff2fb4b1f0cf4fede8f1ab86c45af3869c
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-12 22:28:07 +0530

    some changes in Ex-4.2

--- Week-4/Exercise-4.2/Exercise2.js
+++ Week-4/Exercise-4.2/Exercise2.js
@@ -3,17 +3,20 @@ Person.prototype.initialize = function (name, age) {
     this.name = name;
     this.age = age;
 }
-const Teacher = new Person();
-// we can access .initialize from Person in Teacher.
-console.log("initialize" in Teacher); // true
+class Teacher extends Person {
+    constructor() {
+        super();
+        this.teach = function(subject) {
+            console.log(`${this.name} is now teaching ${subject}`);
+        }; 
+    }
+}
 
-Teacher.teach = function(subject) {return `${this.name} is now teaching ${subject}`;}
 
-//Object Inheritance
-let him = Object.create(Teacher)
+let him = new Teacher();
 
-// can access everithing with object him.
-console.log(him);
 him.initialize("Shubham", 25);
-console.log(him.teach("javaScript"));
+him.teach("javaScript");
+
+
 
