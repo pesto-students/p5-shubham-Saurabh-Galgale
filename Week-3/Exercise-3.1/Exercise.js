255a741671712ae832212897910b8131646e34c5
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-12 20:20:20 +0530

    suggested change's in memoize

--- Week-3/Exercise-3.1/Exercise.js
+++ Week-3/Exercise-3.1/Exercise.js
@@ -4,29 +4,39 @@
 function memoize(fn) {
     const cache = new Map();
     return function (...args) {
-        if (cache.has(args)) {
-            return cache.get(args);
+        args.sort();
+        if (cache.has(JSON.stringify(args))) {
+            console.log("in cache");
+            return cache.get(JSON.stringify(args));
         }
-        cache.set(args, fn(...args));
-        return cache.get(args);
+        const result = fn(...args);
+        cache.set(JSON.stringify(args), fn(...args));
+        console.log(cache);
+        return result;
     }
 }
 
-const add = (a, b) => a + b;
+function add() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
 
 const fnMem = memoize(add);
 
 
 console.time();
-console.log(fnMem(2, 3));
+console.log(fnMem(2, 3, 4, 5, 6));
 console.timeEnd();
 
 console.time();
-console.log(fnMem(3, 100));
+console.log(fnMem(6, 5, 4, 3, 2));
 console.timeEnd();
 
 console.time();
-console.log(fnMem(3, 3));
+console.log(fnMem(2, 6, 3, 5, 4));
 console.timeEnd();
 
 console.time();
