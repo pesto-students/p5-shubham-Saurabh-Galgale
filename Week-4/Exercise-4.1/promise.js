935c8e72e28556ea57d1cd368aa14138ed3658f2
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-12 15:28:10 +0530

    custom promise Exercise

--- Week-4/Exercise-4.1/promise.js
+++ Week-4/Exercise-4.1/promise.js
@@ -1,1 +1,73 @@
-// custom Promise
+function MyPromise(executor) {
+    let onResolve;
+    let onReject;
+    let isFulfilled = false;
+    let isRejected = false;
+    let isCalled = false;
+    let value;
+    let error;
+
+    function resolve(val) {
+        isFulfilled = true;
+        value = val;
+        if (typeof onResolve === 'function' && !isCalled) {
+            onResolve(val);
+            isCalled = true;
+        }
+    }
+
+    function reject(err) {
+        isRejected = true;
+        error = err;
+        if (typeof onReject === 'function' && !isCalled) {
+            onResolve(err);
+            isCalled = true;
+        }
+    }
+
+    this.then = function(thenHandler) {
+        onResolve = thenHandler;
+        if (!isCalled && isFulfilled) {
+            onResolve(value);
+            isCalled = true;
+        }
+        return this;
+    };
+
+    this.catch = function(catchHandler) {
+        onReject = catchHandler;
+        if (!isCalled && isRejected) {
+            onReject(error);
+            isCalled = true;
+        }
+        return this;
+    };
+
+    executor(resolve, reject);
+}
+
+// MyPromise.resolve = (val) => {
+//     return new MyPromise(function executor(resolve, reject) {
+//         resolve(val);
+//     });
+// }
+
+// MyPromise.reject = (reason) => {
+//     return new MyPromise(function executor(resolve, reject) {
+//         reject(reason);
+//     });
+// }
+
+
+
+const getNumber = () => {
+    new MyPromise((res, rej) => {
+        const randomNumber = Math.floor(Math.random() * 1000);
+        if (randomNumber % 5 !== 0) {
+            rej(`Rejected with num:${randomNumber}`);
+        }else
+        res(`Resolve with num: ${randomNumber}`);
+    }).then((x) => console.log(x)).catch((x) => console.log(x));
+}    
+
+getNumber();
