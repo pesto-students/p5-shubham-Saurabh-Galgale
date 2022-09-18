f9be6a676916b8c4e2140c0541b3248eb097c46b
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-18 13:05:21 +0530

    changes in Exercise 3 and 4

---
+++ Week-5/jest-5.4/calculator.js
@@ -0,0 +1,20 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+module.exports.add = add;
+module.exports.multiply = multiply;
+module.exports.subtract = subtract;
+module.exports.divide = divide;  
