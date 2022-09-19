be0ad382bdbddd5196d516ea45ce21799fde511a
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-09-19 13:42:12 +0530

    changes after GD

--- Week-4/Exercise-4.1/promise.js
+++ Week-4/Exercise-4.1/promise.js
@@ -1,62 +1,47 @@
+
 function MyPromise(executor) {
-    let onResolve;
-    let onReject;
     let isFulfilled = false;
     let isRejected = false;
-    let isCalled = false;
     let value;
     let error;
 
+    try{
+        executor(resolve, reject);
+    }catch(e){
+        throw (e);
+    }
+
     function resolve(val) {
+        // console.log("under resolver");
         isFulfilled = true;
         value = val;
-        if (typeof onResolve === 'function' && !isCalled) {
-            onResolve(val);
-            isCalled = true;
-        }
     }
 
     function reject(err) {
+        // console.log("under reject");
         isRejected = true;
         error = err;
-        if (typeof onReject === 'function' && !isCalled) {
-            onResolve(err);
-            isCalled = true;
-        }
     }
 
-    this.then = function(thenHandler) {
-        onResolve = thenHandler;
-        if (!isCalled && isFulfilled) {
-            onResolve(value);
-            isCalled = true;
+    this.then = function (thenHandler, catchHandler) {
+        // console.log("under then");
+        if (isFulfilled) {
+            thenHandler(value);
+        }else if(catchHandler && isRejected) {
+            catchHandler(error);
         }
         return this;
     };
 
-    this.catch = function(catchHandler) {
-        onReject = catchHandler;
-        if (!isCalled && isRejected) {
-            onReject(error);
-            isCalled = true;
+    this.catch = function (catchHandler) {
+        // console.log("under catch");
+        if (isRejected) {
+            catchHandler(error);
         }
         return this;
     };
 
-    executor(resolve, reject);
 }
-
-// MyPromise.resolve = (val) => {
-//     return new MyPromise(function executor(resolve, reject) {
-//         resolve(val);
-//     });
-// }
-
-// MyPromise.reject = (reason) => {
-//     return new MyPromise(function executor(resolve, reject) {
-//         reject(reason);
-//     });
-// }
 
 
 
@@ -65,9 +50,10 @@ const getNumber = () => {
         const randomNumber = Math.floor(Math.random() * 1000);
         if (randomNumber % 5 !== 0) {
             rej(`Rejected with num:${randomNumber}`);
-        }else
-        res(`Resolve with num: ${randomNumber}`);
+        } else
+            res(`Resolve with num: ${randomNumber}`);
     }).then((x) => console.log(x)).catch((x) => console.log(x));
-}    
+}
 
 getNumber();
+
