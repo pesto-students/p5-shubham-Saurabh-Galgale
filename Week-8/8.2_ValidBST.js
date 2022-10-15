159d70ee7db2980f279350669e3f19a64f0cafdf
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2022-10-15 11:01:20 +0530

    validation of BST complete

--- Week-8/8.2_ValidBST.js
+++ Week-8/8.2_ValidBST.js
@@ -1,28 +1,78 @@
 class TreeNode {
     constructor(value) {
-        this.val = value;
+        this.value = value;
         this.left = null;
         this.right = null;
     }
 }
 
+// validate = function (root) {
+//     let currentL = root;
+//     let currentR = root;
+//     while (currentL && currentR) {
+//         if (currentL.left != null && currentL.left.value > currentL.value) {
+//             return false;
+//         } else {
+//             currentL = currentL.left; 
+//         }
+//         if (currentR.right != null && currentR.right.value < currentR.value) {
+//             return false;
+//         } else {
+//             currentR = currentR.right; 
+//         }
+//     }
+//     return true;
+// }
+
+
 validate = function (root) {
-    let currentL = root;
-    let currentR = root;
-    while (currentL && currentR) {
-        if (currentL.left != null && currentL.left.value > currentL.value) {
-            return false;
-        } else {
-            currentL = currentL.left; 
+    let currentL = root.left;
+    let currentR = root.right;
+    let q1 = [];
+    let q2 = [];
+    if (!root) {
+        return "no element found";
+    }
+    q1.push(currentL);
+    q2.push(currentR);
+
+    while (q1) {
+        let temp1 = q1.shift();
+        if (!temp1) { break; }
+        if (temp1) {
+            if (temp1.left != null || temp1.right != null) {
+                if (temp1.value < root.value && temp1.left.value < temp1.value) {
+                    q1.push(temp1.left);
+                } else {
+                    return false;
+                }
+                if (temp1.value < root.value && temp1.right.value > temp1.value && temp1.right.value < root.value) {
+                    q1.push(temp1.right);
+                } else {
+                    // console.log(temp1.value);
+                    // console.log(temp1.right.value);
+                    return false;
+                }
+            }
         }
-        if (currentR.right != null && currentR.right.value < currentR.value) {
-            return false;
-        } else {
-            currentR = currentR.right; 
+    }
+    while (q2) {
+        let temp2 = q2.shift();
+        if (!temp2) { break; }
+        if (temp2) {
+            if (temp2.left != null || temp2.right != null) {
+                if (temp2.value > root.value && temp2.left.value < temp2.value && temp2.left.value > root.value) {
+                    q2.push(temp2.left);
+                } else { return false }
+                if (temp2.value > root.value && temp2.right.value > temp2.value) {
+                    q2.push(temp2.right);
+                } else { return false }
+            }
         }
     }
     return true;
 }
+
 
 
 let root = new TreeNode(50);
